916da92c947e43c05629f7e8641cf46e1a40418d
mdzwink <mdzwink@gmail.com>
2022-07-03 14:01:13 -0400

    updates related to admin_categories and about page links fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/index'
+    get 'categories/new'
+  end
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
@@ -10,12 +14,13 @@ Rails.application.routes.draw do
     post   :add_item
     post   :remove_item
   end
-
+  
   resources :orders, only: [:create, :show]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :show]
   end
 
 
