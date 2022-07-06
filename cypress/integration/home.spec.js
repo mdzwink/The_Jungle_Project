fc13c07edf9ce35d7be2ddad2521ccc4cd36f6aa
mdzwink <mdzwink@gmail.com>
2022-07-06 15:45:34 -0400

    add cypress test for home screen and products showing

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,8 +1,14 @@
 
 
 describe('visit home page', () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
 
-  it('visits home page', () => {
-    cy.visit("/")
-  })
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 })
